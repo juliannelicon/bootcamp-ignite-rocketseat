7839a213098b91f3049422c8e90705adb7f5333c
juliannelicon@gmail.com <juliannelicon@gmail.com>
2022-01-07 14:18:50 -0300

    Projeto ignews

---
+++ ignite-ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,32 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+import { FaGithub } from 'react-icons/fa'
+
+import { FiX } from 'react-icons/fi'
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const [session] = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04D361" />
+      <span>{session.user.name}</span>
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#EBA417" />
+      <span>Sign in with Github</span>
+    </button>
+  )
+}
